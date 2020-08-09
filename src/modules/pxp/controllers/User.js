09f4e262d03944cf6a8fb45a367189c9787e6001
Jaime Rivera <jriverarojas@gmail.com>
2020-08-09 18:07:57 -0400

    Create User.js

---
+++ src/modules/pxp/controllers/User.js
@@ -0,0 +1,35 @@
+const Joi = require('@hapi/joi');
+const { __ } = require('../PxpError');
+const BaseController = require('../BaseController');
+
+class User extends BaseController {
+
+  constructor(method, res, next) {
+    super(method, res, next);
+  }
+
+  async addUser() {
+    this.setMethod('POST');
+    this.setTransaction('SEGU_ADDUSER');
+    //this.setStoreProcedure('pxp.ft_user');
+    this.setModel('pxp/UserModel', 'addUser');
+    //this.setAsync(true);
+    const schema = Joi.object({
+      userName: Joi.string().required(),
+      email: Joi.string().email().required(),
+      password: Joi.string().pattern(/^[a-zA-Z0-9]{3,30}$/).required(),
+    });
+    const params = await __(this.validateSchema(schema));
+
+    // call to database
+    // const a = await __(this.execDB(params));
+
+    //call to Model
+    const a = await __(this.execModel(params));
+    // anything you send here will go inside data object
+    this.response(a);
+  }
+
+}
+
+module.exports = User;

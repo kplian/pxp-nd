97303b26ebdb65e71632b73e390bfeec5561fbef
Jaime Rivera <jriverarojas@gmail.com>
2020-08-09 17:59:00 -0400

    Create BaseModel.js

---
+++ src/lib/BaseModel.js
@@ -0,0 +1,37 @@
+const { PxpError, __ } = require('./PxpError');
+const config = require('./Config');
+class BaseModel {
+
+  constructor() {
+    if (config.database === 'postgres') {
+      this.pool = require('./PgPool');
+    }
+  }
+
+  async exec(transaction, modelFunction, params, checkPermissions = true) {
+    this.transaction = transaction;
+    this.modelFunction = modelFunction;
+    const client = await __(this.pool.connect());
+    let res = {};
+    try {
+      await client.query('BEGIN');
+      if (checkPermissions) {
+        console.log('validar permisos for' + this.transaction);
+      }
+      await eval('res = this.' + this.modelFunction + '(client, params)');
+      await client.query('COMMIT');
+      console.log('async without wait insert to log');
+    } catch (e) {
+      await client.query('ROLLBACK');
+      console.log('sync without wait insert to log');
+      throw e
+    } finally {
+      client.release();
+    }
+    // return res;
+    return { fdafd: 'fdafdsaf', gfdgfds: 'gfsgfsd' }
+  }
+
+}
+
+module.exports = BaseModel;

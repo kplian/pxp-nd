76031ba1ec6dc2aba40662627ead6aeac8645aac
Jaime Rivera <jriverarojas@gmail.com>
2020-09-03 22:01:57 -0400

    PxpEntity

--- src/modules/pxp/entity/PxpEntity.ts
+++ src/modules/pxp/entity/PxpEntity.ts
@@ -4,8 +4,23 @@ import { Entity, BaseEntity, Column, CreateDateColumn } from 'typeorm';
 @Entity({ name: 'tpar_chat', schema: 'pxp' })
 export default class Chat extends BaseEntity {
 
+  @Column({ name: 'created_by' })
+  createdBy: number;
+
+  @Column({ name: 'user_id_ai', nullable: true })
+  userIdAi: number;
+
+  @Column({ name: 'user_ai', type: 'varchar', length: 300, nullable: true })
+  userAi: string;
+
+  @Column({ name: 'modified_by', nullable: true })
+  modifiedBy: number;
+
   @CreateDateColumn({ name: 'created_at' })
   createdAt: Date;
+
+  @Column({ name: 'modified_at', nullable: true })
+  modifiedAt: Date;
 
   @Column({ name: 'is_active', default: true })
   isActive: boolean;

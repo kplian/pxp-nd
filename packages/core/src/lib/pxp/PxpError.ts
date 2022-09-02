000bdc30451436f3d41fb3279477dd7ba71581e0
Jaime Rivera <jaimerivera@Jaimes-MacBook-Pro.local>
2022-09-02 00:39:58 -0400

    add validation for axios error

--- packages/core/src/lib/pxp/PxpError.ts
+++ packages/core/src/lib/pxp/PxpError.ts
@@ -12,28 +12,41 @@
  * Last modified  : 2020-09-20 18:20:43
  */
 
-import express from 'express';
+import express from "express";
 
-const genericMessage = 'Web server error. Contact the administrator';
+const genericMessage = "Web server error. Contact the administrator";
 let showError = false;
 
 class PxpError extends Error {
   statusCode: number;
   message: string;
   tecMessage: string;
-  errorObject: undefined;
-  constructor(statusCode: number, message: string, errorObject?: undefined, stack?: undefined) {
+  errorObject: any;
+  constructor(
+    statusCode: number,
+    message: string,
+    errorObject: any = undefined,
+    stack?: undefined
+  ) {
     super();
     this.statusCode = statusCode;
     this.message = statusCode === 500 ? genericMessage : message;
     this.errorObject = errorObject;
-    this.tecMessage = message;
+
+    if (errorObject && errorObject.response && errorObject.config) {
+      this.tecMessage = `Error in Axios call to ${
+        this.errorObject.config.url
+      }, response: ${JSON.stringify(this.errorObject.response.data)}`;
+    } else {
+      this.tecMessage = message;
+    }
+
     this.stack = stack || this.stack;
   }
 }
 
 // Pxp async promise error handler
-const __ = (promise: Promise<any>, myShowError = false): Promise<any> => (
+const __ = (promise: Promise<any>, myShowError = false): Promise<any> =>
   promise
     .then((data) => data)
     .catch((error) => {
@@ -41,34 +54,36 @@ const __ = (promise: Promise<any>, myShowError = false): Promise<any> => (
       if (error instanceof PxpError) {
         throw error;
       } else {
-        throw new PxpError(showError ? 406 : 500, error.message, undefined, error.stack);
+        throw new PxpError(
+          showError ? 406 : 500,
+          error.message,
+          error,
+          error.stack
+        );
       }
-    })
-);
+    });
 
 // error handler middleware
 const errorMiddleware = (err: PxpError, req: express.Request, res: any) => {
-  const {
-    statusCode, message, stack, tecMessage, errorObject
-  } = err;
+  const { statusCode, message, stack, tecMessage, errorObject } = err;
 
-  const extraObj = process.env.NODE_ENV === 'production' ? {} : { extendedMessage: tecMessage, stack };
+  const extraObj =
+    process.env.NODE_ENV === "production"
+      ? {}
+      : { extendedMessage: tecMessage, stack };
 
   // @todo if production not show tecMessage and stack
-  return res.status(err.statusCode || 500).json(
-    {
-      error: {
-        ...{
-          code: statusCode,
-          message,
-          logId: res.logId ? res.logId : undefined,
-          errorObject,
-        },
-        ...extraObj,
+  return res.status(err.statusCode || 500).json({
+    error: {
+      ...{
+        code: statusCode,
+        message,
+        logId: res.logId ? res.logId : undefined,
+        errorObject,
       },
+      ...extraObj,
     },
-  );
-
+  });
 };
 
 export { PxpError, errorMiddleware, __ };
